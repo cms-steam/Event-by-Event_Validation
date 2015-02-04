6ac268bb7feb3d07dfe76e51a1b2e8a1081253be
Charlie Mueller <charles.mueller@cern.ch>
2015-02-04 11:25:44 +0100

    updates to naming

--- HLTEventByEventComparison/test/compare_hists.py
+++ HLTEventByEventComparison/test/compare_hists.py
@@ -10,14 +10,18 @@ def StripVersion(name):
 
 #def main():
 if __name__=='__main__':
-
-    file0 = '/afs/cern.ch/user/m/muell149/work/HLTONLINE/CMSSW_7_3_0/src/DQMOffline/Trigger/test/723_stage1.root'
-    file1 = '/afs/cern.ch/user/m/muell149/work/HLTONLINE/CMSSW_7_3_0/src/DQMOffline/Trigger/test/730_stage1.root'
-
-    file_0 = TFile(file0)
-    file_1 = TFile(file1)
-    hist0 = file_0.Get('newHLTOffline/hlt_count_hist')
-    hist1 = file_1.Get('newHLTOffline/hlt_count_hist')#eventbyevent/HLT_Tgt')
+   
+   #input files
+   fileRef = '/afs/cern.ch/user/m/muell149/work/HLTONLINE/CMSSW_7_3_0/src/DQMOffline/Trigger/test/723_stage1.root'
+   fileTgt = '/afs/cern.ch/user/m/muell149/work/HLTONLINE/CMSSW_7_3_0/src/DQMOffline/Trigger/test/730_stage1.root'
+   #Title strings for target and reference
+   target = 'tgt_CMSSW_REL'
+   reference ='ref_CMSSW_REL'
+   
+    file_ref = TFile(fileRef)
+    file_tgt = TFile(fileTgt)
+    hist_ref = file_ref.Get('newHLTOffline/hlt_count_hist')
+    hist_tgt = file_tgt.Get('newHLTOffline/hlt_count_hist')#eventbyevent/HLT_Tgt')
     
     output_log = open('summary_table.csv',"w")
     output_log.write("PATH, COUNTS IN REFERENCE, COUNTS IN TARGET, abs(TGT-REF)/REF, ERROR\n")
@@ -29,26 +33,26 @@ if __name__=='__main__':
     table = []
     row = []
 
-    bins = hist0.GetNbinsX()
+    bins = hist_ref.GetNbinsX()
 
-    nhist0 = TH1F("counts","counts",bins+1,0,bins+1)
-    nhist1 = TH1F("counts","counts",bins+1,0,bins+1)
+    nhist_ref = TH1F("counts","counts",bins+1,0,bins+1)
+    nhist_tgt = TH1F("counts","counts",bins+1,0,bins+1)
     
     for bin in xrange(bins):
-        bl0 = hist0.GetXaxis().GetBinLabel(bin)
-        bl1 = hist1.GetXaxis().GetBinLabel(bin)
+        bl0 = hist_ref.GetXaxis().GetBinLabel(bin)
+        bl1 = hist_tgt.GetXaxis().GetBinLabel(bin)
 
-        bin_dict0[StripVersion(bl0)]=hist0.GetBinContent(bin)
-        bin_dict1[StripVersion(bl1)]=hist1.GetBinContent(bin)
+        bin_dict0[StripVersion(bl0)]=hist_ref.GetBinContent(bin)
+        bin_dict1[StripVersion(bl1)]=hist_tgt.GetBinContent(bin)
 
     i=1
     for key in bin_dict0:
        if bin_dict1.has_key(key):
             print "key == ", key   
-            nhist0.Fill(i,bin_dict0[key])
-            nhist1.Fill(i,bin_dict1[key])
-            nhist0.GetXaxis().SetBinLabel(i+1,key)
-            nhist1.GetXaxis().SetBinLabel(i+1,key)
+            nhist_ref.Fill(i,bin_dict0[key])
+            nhist_tgt.Fill(i,bin_dict1[key])
+            nhist_ref.GetXaxis().SetBinLabel(i+1,key)
+            nhist_tgt.GetXaxis().SetBinLabel(i+1,key)
        
             if bin_dict0[key] != bin_dict1[key]:
                print "key = ",key
@@ -75,7 +79,7 @@ if __name__=='__main__':
     for row in table:
        output_log.write("%(1)s,%(2)s,%(3)s,%(4)s,%(5)s\n"%{"1":row[0],"2":row[1],"3":row[2],"4":row[3],"5":row[4]})
 
-    title = nhist0.GetName()
+    title = nhist_ref.GetName()
     can = TCanvas(title,title,5000,3000)
     pad1 = TPad("pad1","pad1",0,0.28,1,1)
     leg = TLegend(0.73,0.66,0.90,0.8)
@@ -83,19 +87,19 @@ if __name__=='__main__':
     pad1.SetBottomMargin(0)
     pad1.Draw()
     pad1.cd()
-    nhist0.SetStats(0)
-    nhist1.SetStats(0)
-    nhist1.SetLineColor(4)
-    nhist0.SetLineColor(2)
-    leg.AddEntry(nhist0,'CMSSW_7_2_3')
-    leg.AddEntry(nhist1,'CMSSW_7_3_0')
-    #nhist0.LabelsOption("a")
-    nhist0.SetLabelSize(0.02)
-    #nhist1.LabelsOption("a")
-    nhist1.SetLabelSize(0.02)
+    nhist_ref.SetStats(0)
+    nhist_tgt.SetStats(0)
+    nhist_tgt.SetLineColor(4)
+    nhist_ref.SetLineColor(2)
+    leg.AddEntry(nhist_ref,target)
+    leg.AddEntry(nhist_tgt,reference)
+    #nhist_ref.LabelsOption("a")
+    nhist_ref.SetLabelSize(0.02)
+    #nhist_tgt.LabelsOption("a")
+    nhist_tgt.SetLabelSize(0.02)
 
-    nhist1.DrawCopy()
-    nhist0.Draw("same")
+    nhist_tgt.DrawCopy()
+    nhist_ref.Draw("same")
     leg.Draw("same")
     can.cd()
         
@@ -104,19 +108,19 @@ if __name__=='__main__':
     pad2.SetBottomMargin(0.34)
     pad2.Draw()
     pad2.cd()
-    nhist1.Sumw2()
-    nhist1.SetTitle("")
-    nhist1.SetStats(0)
-    nhist1.Add(nhist0,-1)
-    nhist1.Divide(nhist0)
-    nhist1.GetYaxis().SetTitle("New-Old/Old")
+    nhist_tgt.Sumw2()
+    nhist_tgt.SetTitle("")
+    nhist_tgt.SetStats(0)
+    nhist_tgt.Add(nhist_ref,-1)
+    nhist_tgt.Divide(nhist_ref)
+    nhist_tgt.GetYaxis().SetTitle("New-Old/Old")
  
-  #nhist0.SetMarkerStyle(21)
-    nhist1.SetLineColor(1)
-    nhist1.GetYaxis().CenterTitle()
-    nhist1.GetYaxis().SetTitleSize(.065)
-    nhist1.GetYaxis().SetTitleOffset(.4)
-    nhist1.Draw("ep")
+  #nhist_ref.SetMarkerStyle(21)
+    nhist_tgt.SetLineColor(1)
+    nhist_tgt.GetYaxis().CenterTitle()
+    nhist_tgt.GetYaxis().SetTitleSize(.065)
+    nhist_tgt.GetYaxis().SetTitleOffset(.4)
+    nhist_tgt.Draw("ep")
     can.SaveAs("menu_changes_by_path.png")
     #can.cd()
   
